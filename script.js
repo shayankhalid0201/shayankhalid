f88e9e30b14c5e48a6d8a7f03ae8e097701dfbae
shayankhalid0201 <132909462+shayankhalid0201@users.noreply.github.com>
2023-08-14 22:05:01 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Slideshow Code
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  const slides = document.querySelectorAll(".mySlides");
+  
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  
+  slideIndex++;
+  
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  
+  slides[slideIndex - 1].style.display = "block";
+  
+  setTimeout(showSlides, 1500); // Change slide every 1.5 seconds
+}
+
+// Mobile Navigation Toggle
+function myFunction() {
+  const topnav = document.getElementById("myTopnav");
+  if (topnav.className === "topnav") {
+    topnav.className += " responsive";
+  } else {
+    topnav.className = "topnav";
+  }
+}
